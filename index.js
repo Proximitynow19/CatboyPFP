4c192a5904b4c2521501a0364e1b3013c03e59b1
Jakob de Guzman <55227471+Proximitynow19@users.noreply.github.com>
2022-12-23 19:09:19 +1300

    repeat every 10m

--- index.js
+++ index.js
@@ -12,37 +12,45 @@ const { REST } = require("@discordjs/rest");
 
 const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
 
-(async () => {
-  const src = (await catboy.image()).url;
+const setAvatar = async () => {
+  try {
+    const src = (await catboy.image()).url;
 
-  const res = await fetch(src);
+    const res = await fetch(src);
 
-  const type = res.headers["content-type"];
+    const type = res.headers["content-type"];
 
-  const buff = await res.buffer();
+    const buff = await res.buffer();
 
-  smartcrop
-    .crop(buff, { width: 128, height: 128 })
-    .then(async function (result) {
-      const crop = result.topCrop;
+    smartcrop
+      .crop(buff, { width: 128, height: 128 })
+      .then(async function (result) {
+        const crop = result.topCrop;
 
-      const outBuff = await sharp(buff)
-        .extract({
-          width: crop.width,
-          height: crop.height,
-          left: crop.x,
-          top: crop.y,
-        })
-        .resize(128, 128)
-        .toBuffer();
+        const outBuff = await sharp(buff)
+          .extract({
+            width: crop.width,
+            height: crop.height,
+            left: crop.x,
+            top: crop.y,
+          })
+          .resize(128, 128)
+          .toBuffer();
 
-      await rest.patch("/users/@me", {
-        body: {
-          avatar: `data:${type};base64,${outBuff.toString("base64")}`,
-        },
-        authPrefix: "",
+        await rest.patch("/users/@me", {
+          body: {
+            avatar: `data:${type};base64,${outBuff.toString("base64")}`,
+          },
+          authPrefix: "",
+        });
+
+        console.log(`Done!`);
       });
+  } catch (e) {}
+};
 
-      console.log(`Done!`);
-    });
-})();
+setAvatar();
+
+setInterval(async () => {
+  await setAvatar();
+}, 10 * 60 * 1000 + 1);
